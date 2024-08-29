9340988527c52a6dd1aaec68b444e9074720d5d8
Guilherme Silva <guissilva_@outlook.com>
2024-08-29 19:51:40 -0300

    FEATURE - adicionado bootstrap ao projeto

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,5 +2,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 	<title>Gerenciador de Condomínio</title>
 </head>
